5a76bf07aed93510687c355206d629653a1222c5
wilson <wilson-gs@hotmail.com>
2024-02-09 19:44:00 -0300

    Obtendo um post por id

---
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.nelioalves.workshopmongo.resources;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
+import com.nelioalves.workshopmongo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok(obj);
+	}
+
+}
---
+++ src/main/java/com/nelioalves/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.nelioalves.workshopmongo.service;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
+import com.nelioalves.workshopmongo.repository.PostRepository;
+import com.nelioalves.workshopmongo.repository.UserRepository;
+import com.nelioalves.workshopmongo.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
